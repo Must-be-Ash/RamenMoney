c35aa1ba746bb613d9cb5776fc0a84f711b47423
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-09-15 20:56:25 -0700

    Your commit message here

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+# Remove or comment out the following lines:
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5001)
+
+# Add this line at the end of the file
+app = app
